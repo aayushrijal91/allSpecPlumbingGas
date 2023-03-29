61a10e303fd549767ce91768d17a8f5a49f54045
Aayush Rijal <arijal@aiims.com.au>
2023-03-29 15:42:56 +1100

    Form done

---
+++ app/README.md
@@ -0,0 +1,22 @@
+# Landing page [All Spec Plumbing & Gas](https://www.aiims.com.au)
+
+This is a landing page boilerplate that uses PHP scripting language
+
+## _Author Information_
+
+Developer: [Aayush Rijal](https://www.aayushrijal.net)
+
+Contact: 0452637371
+
+[Github](https://github.com/aayushrijal91/allSpecPlumbingGas)
+
+[Figma](https://www.figma.com/file/ogRhJbWmpE5uJmGPUkEDkm/All-Spec?node-id=0-1&t=LNTs5Q3WnElLpAfN-0)
+
+## _Instruction_
+
+```bash
+ npm install
+ npm start
+ ```
+
+Reference for full repository
---
+++ app/src/main.php
@@ -0,0 +1,234 @@
+<header>
+    <div class="navigation py-3">
+        <div class="container">
+            <div class="row justify-content-center justify-content-lg-between align-items-center">
+                <div class="col-auto">
+                    <a href="./">
+                        <?= renderImg("logo.png", "logo") ?>
+                    </a>
+                </div>
+                <div class="col-auto">
+                    <div class="row align-items-center gx-xl-5">
+                        <div class="col-auto d-flex align-items-center gap-3">
+                            <div><?= renderImg('industry-years.png', 'icons') ?></div>
+                            <div class="fs-14 lh-1 text-white">
+                                Expertise: <span class="fw-700">Industry<br> for 10+ years</span>
+                            </div>
+                        </div>
+                        <div class="col-auto d-flex align-items-center gap-3">
+                            <div><?= renderImg('thumbsup.png', 'icons') ?></div>
+                            <div class="fs-14 lh-1 text-white">
+                                <div class="fw-700">Expert solutions,</div> exceptional service
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="row align-items-center bg-white rounded-pill gx-0">
+                                <div class="col-12 col-md-auto">
+                                    <a href="#form" class="btn rounded-0 text-secondary py-3 ps-5 pe-4">
+                                        Contact Us
+                                    </a>
+                                </div>
+                                <div class="col-12 col-md-auto">
+                                    <a href="tel:<?= $phone_number ?>" class="btn btn-secondary rounded-pill text-white py-3 px-md-5">
+                                        Call Us <span class="ps-2 fw-700"><?= $phone_number ?></span>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="banner">
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-lg-5 text-white">
+                    <h1 class="font-articulat-heavy fs-58 lh-1 fw-800">Reliable plumbing solutions, <span class="text-secondary">every time</span></h1>
+                    <div class="fs-18 pb-3">Pellentesque habitant morbi tristique senectus et netus et malesuada.</div>
+                    <div class="row justify-content-between py-4 algin-items-center">
+                        <div class="col-4">
+                            <div class="">Clients & Brands</div>
+                            <div id="client-slider">
+                                <div><?= renderImg('client-1.png', 'logo') ?></div>
+                                <div><?= renderImg('client-1.png', 'logo') ?></div>
+                            </div>
+                        </div>
+                        <div class="col-8">
+                            <div class="gradient-line"></div>
+                            <div class="py-3 text-center text-white fs-29 lh-1">
+                                Refer a friend and get
+                                <div class="text-secondary fw-700">$65 off next plumbing job</div>
+                            </div>
+                            <div class="gradient-line"></div>
+                        </div>
+                    </div>
+
+                    <div class="banner_box_1">
+                        <div class="cheque"><?= renderImg('cheque.png', 'icons') ?></div>
+                        CCTV Drain Inspections Starting <div class="text-secondary">From $99*</div>
+                    </div>
+
+                    <div class="row justify-content-between position-absolute">
+                        <div class="col-auto">
+                            <div class="cta-card">
+                                <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M29.3475 5.99999C25.9568 5.99999 20.5415 3.18399 16.9475 0.399986C16.2597 -0.136014 15.3103 -0.136014 14.6225 0.399986C11.0284 3.18399 5.6131 5.99999 2.22247 5.99999C1.15297 5.99999 0.284973 6.89399 0.284973 7.99999C0.284973 19.168 5.51816 27.634 15.0216 31.838C15.2657 31.946 15.5253 32 15.785 32C16.0446 32 16.3042 31.946 16.5483 31.838C26.0518 27.634 31.285 19.168 31.285 7.99999C31.285 6.89399 30.417 5.99999 29.3475 5.99999ZM15.785 27.808V16H5.35347C4.73735 14.096 4.34404 12.038 4.21229 9.81399C7.94779 9.16399 12.3672 6.88999 15.785 4.45999V16H26.2165C24.5153 21.268 21.0046 25.286 15.785 27.808Z" fill="white" />
+                                </svg>
+                                <div><span class="fw-700">Life Time</span><br>Warranty</div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="cta-card">
+                                <svg width="28" height="27" viewBox="0 0 28 27" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g clip-path="url(#clip0_267_30)">
+                                        <path d="M21.9641 27H1.71405C0.70155 27 0.0265503 26.325 0.0265503 25.3125V1.6875C0.0265503 0.675 0.70155 0 1.71405 0H21.9641V3.375H3.40155V23.625H20.2766V21.9375H23.6516V25.3125C23.6516 26.325 22.9766 27 21.9641 27Z" fill="white" />
+                                        <path d="M27.0266 10.125C27.0266 7.25625 24.8328 5.0625 21.9641 5.0625C19.0953 5.0625 16.9016 7.25625 16.9016 10.125C16.9016 11.6438 17.5766 12.9937 18.5891 13.8375V20.25L21.9641 16.875L25.3391 20.25V13.8375C26.3516 12.9937 27.0266 11.6438 27.0266 10.125Z" fill="white" />
+                                        <path d="M13.5266 8.4375H6.77655V11.8125H13.5266V8.4375Z" fill="white" />
+                                        <path d="M15.2141 15.1875H6.77655V18.5625H15.2141V15.1875Z" fill="white" />
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_267_30">
+                                            <rect width="27" height="27" fill="white" transform="translate(0.0265503)" />
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+
+                                <div><span class="fw-700">Licensed</span> &<br>Injured</div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="cta-card">
+                                <svg width="29" height="28" viewBox="0 0 29 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <g clip-path="url(#clip0_267_43)">
+                                        <path d="M14.2681 28C11.4992 28 8.79244 27.1789 6.49015 25.6406C4.18786 24.1022 2.39345 21.9157 1.33382 19.3576C0.274193 16.7994 -0.00305331 13.9845 0.53714 11.2687C1.07733 8.55301 2.4107 6.05845 4.36864 4.10051C6.32657 2.14258 8.82114 0.809206 11.5369 0.269012C14.2526 -0.271181 17.0675 0.006066 19.6257 1.06569C22.1839 2.12532 24.3704 3.91973 25.9087 6.22202C27.4471 8.52431 28.2681 11.2311 28.2681 14C28.2644 17.7119 26.7882 21.2707 24.1635 23.8954C21.5388 26.5201 17.98 27.9963 14.2681 28ZM14.2681 4.66667C12.4222 4.66667 10.6177 5.21406 9.08281 6.23962C7.54795 7.26518 6.35168 8.72285 5.64526 10.4283C4.93884 12.1337 4.75401 14.0104 5.11414 15.8209C5.47427 17.6313 6.36318 19.2944 7.66847 20.5997C8.97376 21.905 10.6368 22.7939 12.4473 23.154C14.2578 23.5141 16.1344 23.3293 17.8398 22.6229C19.5453 21.9165 21.003 20.7202 22.0285 19.1853C23.0541 17.6505 23.6015 15.846 23.6015 14C23.6015 11.5247 22.6181 9.15068 20.8678 7.40034C19.1175 5.65 16.7435 4.66667 14.2681 4.66667Z" fill="white" />
+                                        <path d="M20.1015 15.1667H14.2681C13.9587 15.1667 13.662 15.0438 13.4432 14.825C13.2244 14.6062 13.1015 14.3094 13.1015 14V8.16667C13.1015 7.85725 13.2244 7.5605 13.4432 7.34171C13.662 7.12292 13.9587 7 14.2681 7C14.5775 7 14.8743 7.12292 15.0931 7.34171C15.3119 7.5605 15.4348 7.85725 15.4348 8.16667V12.8333H20.1015C20.4109 12.8333 20.7076 12.9563 20.9264 13.175C21.1452 13.3938 21.2681 13.6906 21.2681 14C21.2681 14.3094 21.1452 14.6062 20.9264 14.825C20.7076 15.0438 20.4109 15.1667 20.1015 15.1667Z" fill="white" />
+                                    </g>
+                                    <defs>
+                                        <clipPath id="clip0_267_43">
+                                            <rect width="28" height="28" fill="white" transform="translate(0.268127)" />
+                                        </clipPath>
+                                    </defs>
+                                </svg>
+
+                                <div><span class="fw-700">24 Hour</span><br>Response Time</div>
+                            </div>
+                        </div>
+                        <div class="col-auto">
+                            <div class="cta-card">
+                                <svg width="28" height="26" viewBox="0 0 28 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M23.0098 4.33333V1.08333C23.0098 0.796016 22.8912 0.520465 22.6803 0.317301C22.4693 0.114137 22.1831 0 21.8848 0L19.6348 0C19.3364 0 19.0502 0.114137 18.8393 0.317301C18.6283 0.520465 18.5098 0.796016 18.5098 1.08333V4.33333H9.50977V1.08333C9.50977 0.796016 9.39124 0.520465 9.18026 0.317301C8.96928 0.114137 8.68313 0 8.38477 0L6.13477 0C5.8364 0 5.55025 0.114137 5.33927 0.317301C5.12829 0.520465 5.00977 0.796016 5.00977 1.08333V4.33333C3.81629 4.33333 2.6717 4.78988 1.82779 5.60254C0.983871 6.41519 0.509766 7.5174 0.509766 8.66667L0.509766 21.6667C0.509766 22.8159 0.983871 23.9181 1.82779 24.7308C2.6717 25.5435 3.81629 26 5.00977 26H23.0098C24.2032 26 25.3478 25.5435 26.1917 24.7308C27.0357 23.9181 27.5098 22.8159 27.5098 21.6667V8.66667C27.5098 7.5174 27.0357 6.41519 26.1917 5.60254C25.3478 4.78988 24.2032 4.33333 23.0098 4.33333ZM23.0098 21.6667H5.00977V10.8333H23.0098V21.6667Z" fill="white" />
+                                </svg>
+
+                                <div><span class="fw-700">Same Day</span><br>Response</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 position-relative">
+                    <div class="inspection_slider flame_1 active">
+                        <div>
+                            <div class="text-white fs-51 fw-700 lh-1">Free <span class="text-primary">Home Inspection</span></div>
+                            <div class="text-white pt-3 pb-4">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</div>
+                            <a href="#form" class="btn btn-white d-inline-flex text-secondary rounded-pill px-xl-5">Contact Us</a>
+                        </div>
+                    </div>
+                    <div class="inspection_slider flame_2">
+                        <div>
+                            <div class="text-secondary fs-51 fw-700 lh-1">Ph Water<br> Testing <span class="text-white">For $60</span></div>
+                            <div class="text-white pt-3 pb-4">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</div>
+                            <a href="#form" class="btn btn-white d-inline-flex text-secondary rounded-pill px-xl-5">Contact Us</a>
+                        </div>
+                    </div>
+                    <div class="inspection_slider flame_3">
+                        <div>
+                            <div class="text-white fs-51 fw-700 lh-1">Free <span class="text-primary">Roof Inspection</span></div>
+                            <div class="text-white pt-3 pb-4">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</div>
+                            <a href="#form" class="btn btn-white d-inline-flex text-secondary rounded-pill px-xl-5">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<section class="form">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-8">
+                <div class="fs-64 fw-800 text-black text-center lh-1 pb-2"><span class="text-primary">Affordable plumbing</span> solutions for every budget</div>
+                <div class="fs-18 text-dark text-center lh-2 py-4">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.<br> Phasellus et finibus metus, malesuada varius neque. Etiam tincidunt ornare lectus non condimentum.</div>
+            </div>
+        </div>
+
+        <div class="row pt-5 gx-5">
+            <div class="col-lg-6">
+                <div class="form_box_wrapper">
+                    <div class="form_box">
+                        <div class="cheque"><?= renderImg('cheque.png', 'icons') ?></div>
+                        <div class="text-primary">7 Years Guarantee <span class="text-secondary">On All Works Done</span></div>
+                        <div class="fs-16 fw-500 text-quarternary">Pellentesque habitant morbi tristique senectus et netus et malesuada.</div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <form action="./src/form" method="POST">
+                    <div class="row gy-3">
+                        <div class="col-12">
+                            <input type="text" class="form-control" placeholder="Your Full Name" name="fullName" required />
+                        </div>
+                        <div class="col-md-6">
+                            <input type="email" class="form-control" placeholder="Your Email" name="email" required />
+                        </div>
+                        <div class="col-md-6">
+                            <input type="tel" class="form-control" placeholder="Your Number" name="phoneNumber" required />
+                        </div>
+                        <div class="col-12">
+                            <select name="service" class="form-control" required>
+                                <option selected disabled>Service Required</option>
+                                <option>Service 1</option>
+                                <option>Service 2</option>
+                                <option>Service 3</option>
+                                <option>Service 4</option>
+                            </select>
+                        </div>
+                        <div class="col-12">
+                            <input type="text" name="message" class="form-control" placeholder="Your Message" />
+                        </div>
+                        <div class="col-12">
+                            <button type="submit" class="btn form-control text-white">Submit</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer>
+    <div class="container">
+        <div class="row justify-content-center justify-content-lg-between align-items-center">
+            <div class="col-auto">
+                <div class="row gx-2 gx-md-3 text-white justify-content-center">
+                    <div class="col-12 text-center pb-3 d-lg-none">
+                        <a href="https://www.aiims.com.au/like-our-work/" target="_blank">
+                            <?= renderImg("aiims.png", "logo") ?>
+                        </a>
+                    </div>
+                    <div class="col-auto text-center">Copyright <?= date('Y') ?></div>
+                    <div class="col-auto">|</div>
+                    <div class="col-auto text-center"><?= $site ?></div>
+                    <div class="col-auto">|</div>
+                    <div class="col-auto text-center">All Rights Reserved</div>
+                </div>
+            </div>
+            <div class="col-auto d-none d-lg-block">
+                <a href="https://www.aiims.com.au/like-our-work/" target="_blank">
+                    <?= renderImg("aiims.png", "logo") ?>
+                </a>
+            </div>
+        </div>
+    </div>
+</footer>
