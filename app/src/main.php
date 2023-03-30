f0855f64633069a607731088958d9e224b49df4c
Aayush Rijal <arijal@aiims.com.au>
2023-03-30 17:05:52 +1100

    Form linking done

--- app/src/main.php
+++ app/src/main.php
@@ -217,7 +217,7 @@
     </div>
 </header>
 
-<section class="form">
+<section class="form" id="form">
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-lg-8">
